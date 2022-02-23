1c16aa78d96976125a3f10808d48fdb8b4c1e2bc
zkiss97 <zkiss1997@gmail.com>
2022-02-23 09:30:13 +0100

    Második és harmadik is működik.
    a programok közt kommentes elválasztás van, illetve a feladatok címe alatt kezdődik blokk komment, azokat ki kell venni ha futtatni akarjuk. egyszerre csak egy fut

--- CS_dolgozat/Program.cs
+++ CS_dolgozat/Program.cs
@@ -12,7 +12,7 @@ namespace CS_dolgozat
         {
             // Negatív
 
-
+            /*
             int egyik;
             int masik;
 
@@ -30,6 +30,70 @@ namespace CS_dolgozat
                 Console.WriteLine("Csak a második szám negatív");
             if ((egyik > 0) && (masik > 0))
                 Console.WriteLine("Egyik szám sem negatív");
+            */
+
+            // Console.WriteLine();
+            // Console.ReadLine();
+
+            /***************************************************************************************************/
+            /***************************************************************************************************/
+            /***************************************************************************************************/
+
+            // 2. Könyv
+
+            /*
+            int pages;
+            string title;
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("Add meg a könyv címét: ");
+                title = Console.ReadLine();
+
+                Console.WriteLine("Add meg az oldalszámot: ");
+                pages = int.Parse(Console.ReadLine());
+
+                if (pages < 150)
+                    Console.WriteLine("A(z) " + title + " egy rövid terjedelmű könyv.\n");
+                if (pages >= 150)
+                    Console.WriteLine("A(z) " + title + " egy hosszú terjedelmű könyv.\n");
+            }
+            */
+
+            /***************************************************************************************************/
+            /***************************************************************************************************/
+            /***************************************************************************************************/
+
+            // 3. Született
+
+            /*
+            string[] names = new string[3];
+            string[] jobs = new string[3];
+            string[] genders = new string[3];
+            Random rnd = new Random();
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("Add meg a nevet: ");
+                names[i] = Console.ReadLine();
+                
+                Console.WriteLine("Add meg a foglalkozást: ");
+                jobs[i] = Console.ReadLine();
+                
+                Console.WriteLine("Add meg a nemet(f/n): ");
+                genders[i] = Console.ReadLine();
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+                if (genders[i] == "n")
+                    Console.WriteLine(names[i] + " egy " + jobs[i] + " nő, szerencseszáma: " + rnd.Next(1, 50));
+                if (genders[i] == "f")
+                    Console.WriteLine(names[i] + " egy " + jobs[i] + " férfi, szerencseszáma: " + rnd.Next(1, 50));
+            }
+
+            */
+
 
             Console.ReadLine();
         }
