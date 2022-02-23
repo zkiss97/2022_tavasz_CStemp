3af510cd451f9e1bd6a0d74eb17e31d8e68d8c3f
zkiss97 <zkiss1997@gmail.com>
2022-02-23 08:57:46 +0100

    Első feladat pipa

---
+++ CS_dolgozat/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_dolgozat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Negatív
+
+
+            int egyik;
+            int masik;
+
+            Console.WriteLine("Add meg az egyik számot: ");
+            egyik = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Add meg az egyik számot: ");
+            masik = int.Parse(Console.ReadLine());
+
+            if ((egyik < 0) && (masik < 0))
+                Console.WriteLine("Mindkét szám negatív");
+            if ((egyik < 0) && (masik > 0))
+                Console.WriteLine("Csak az első szám negatív");
+            if ((egyik > 0) && (masik < 0))
+                Console.WriteLine("Csak a második szám negatív");
+            if ((egyik > 0) && (masik > 0))
+                Console.WriteLine("Egyik szám sem negatív");
+
+            Console.ReadLine();
+        }
+    }
+}
